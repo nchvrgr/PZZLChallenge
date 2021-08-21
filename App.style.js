55fbea87bb3745c2ede4ae7523699e7cf12f2aa1
nchvrgr <nachotoasies@gmail.com>
2021-08-20 21:21:06 -0300

    first commit

---
+++ App.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+      container: {
+            flex: 1,
+            backgroundColor: 'gray',
+            color: 'white',
+            alignItems: 'center',
+            justifyContent: 'center'
+      },
+      text: {
+            color: 'white',
+            fontSize: 25
+      }
+
+})
+
+
+  
